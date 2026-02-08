71c5d960533c8455a11f4ff11d333175844f3c3f
Matvei-Kazyukevich <2005mmk@gmail.com>
2026-02-08 21:44:18 +0300

    Add imports

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,2 +1,5 @@
 from app.db.models.base import Base
 from app.db.models.document import Document
+from app.db.models.rule import Rule
+from app.db.models.rule_execution import RuleExecution
+from app.db.models.audit_finding import AuditFinding
